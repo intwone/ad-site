891cee8464e2b5c2fad4662edacdd5bbb55c664b
Cassio Oliveira Silva <cassiointw1993@gmail.com>
2019-08-16 19:26:16 -0300

    :tada: first commit

---
+++ public/index.php
@@ -0,0 +1,179 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>B&S Ads</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../src/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../src/assets/css/style.css">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  </head>
+  <body>
+    <div class="container-fluid">
+      <div class="navbar navbar-expand-lg navbar-light" style="background-color: rgb(113, 89, 193)">
+        <div class="brand">
+          <img src="../src/assets/images/brand.png" alt="">
+        </div>
+
+        <div class="divisor"></div>
+
+        <div class="menu-items">
+          <div class="style-text"><a href="">Meus anúncios</a></div>
+          <div class="style-text"><a href="">Ajuda</a></div>
+          <div class="style-text"><a href="">Chat</a></div>
+          <div class="style-text"><a href="">Minha Conta</a></div>
+        </div>
+
+        <div class="style-button">
+          <div><Button>Login</Button></div>
+          </div><i class="fa fa-sign-in-alt"></i></div>
+        </div>
+      </div>
+
+      <div class="search-center"> 
+        <form class="search-products" action="">
+          <input type="text" name="" placeholder="Estou procurando por...">
+          <a href="#"><img class="images-category" src="../src/assets/images/search.png" alt=""></a>
+        </form>
+      </div>
+
+      <div class="category">
+        <div class="icon-category">
+          <div>
+            <img class="images-category" src="../src/assets/images/house.png" alt="">
+          </div>
+          <div class="title">
+            <small>Imóveis</small>
+          </div>
+        </div>
+
+        <div class="icon-category">
+          <div>
+            <img class="images-category" src="../src/assets/images/car.png" alt="">
+          </div>
+          <div class="title">
+            <small>Autos e peças</small>
+          </div>
+        </div>
+
+        <div class="icon-category">
+          <div>
+            <img class="images-category" src="../src/assets/images/bed.png" alt="">
+          </div>
+          <div class="title">
+            <small>Para sua casa</small>
+          </div>
+        </div>
+
+        <div class="icon-category">
+          <div>
+            <img class="images-category" src="../src/assets/images/eletronic.png" alt="">
+          </div>
+          <div class="title">
+            <small>Eletrônicos</small>
+          </div>
+        </div>
+
+        <div class="icon-category">
+          <div>
+            <img class="images-category" src="../src/assets/images/tools.png" alt="">
+          </div>
+          <div class="title">
+            <small>Serviços</small>
+          </div>
+        </div>
+
+        <div class="icon-category">
+          <div>
+            <img class="images-category" src="../src/assets/images/guitar.png" alt="">
+          </div>
+          <div class="title">
+            <small>Música & hobbies</small>
+          </div>
+        </div>
+
+        <div class="icon-category">
+          <div>
+            <img class="images-category" src="../src/assets/images/cyclist.png" alt="">
+          </div>
+          <div class="title">
+            <small>Esportes</small>
+          </div>
+        </div>
+
+        <div class="icon-category">
+          <div>
+            <img class="images-category" src="../src/assets/images/laundry.png" alt="">
+          </div>
+          <div class="title">
+            <small>Moda e beleza</small>
+          </div>
+        </div>
+
+        <div class="icon-category">
+          <div>
+            <img class="images-category" src="../src/assets/images/tractor.png" alt="">
+          </div>
+          <div class="title">
+            <small>Agro e indústria</small>
+          </div>
+        </div>
+
+        <div class="icon-category">
+          <div>
+            <img class="images-category" src="../src/assets/images/employee.png" alt="">
+          </div>
+          <div class="title">
+            <small>Emprego</small>
+          </div>
+        </div>
+      </div>
+
+
+
+      <div class="box-ads">
+        <div class="list-ads">
+
+        </div>
+      </div>
+
+
+      
+      <footer>
+        <section>
+          <div class="title-sendemail">
+            <p>Recebe todos os anúncios da plataforma.</p>
+          </div>
+          <form class="send-email" action="">
+            <input type="text" name="" placeholder="Digite seu email">
+            <a href="#"><img class="images-category" src="../src/assets/images/send-button.png" alt=""></a>
+          </form>
+
+          <div class="social">
+            <div><a href="#"><i class="fa fa-facebook-f"></i></a></div>
+            <div><a href="#"><i class="fa fa-instagram"></i></a></div>
+            <div><a href="#"><i class="fa fa-twitter"></i></a></div>
+            <div><a href="#"><i class="fa fa-youtube"></i></a></div>
+            <div><a href="#"><i class="fa fa-linkedin"></i></a></div>
+          </div>
+        </section>
+        <p>
+          <strong>B&S Ads ©</strong>
+          2019 - Todos os direitos reservados
+        </p>
+      </footer>
+    </div>
+      
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="../src/assets/js/jquery.min.js"></script>
+    <script src="../src/assets/js/bootstrap.bundle.min.js"></script>
+    <script src="../src/assets/js/script.js"></script>
+  </body>
+</html>
+
