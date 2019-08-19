d4444930a463226dd038cdfdc42e754fe32da613
Cassio Oliveira Silva <cassiointw1993@gmail.com>
2019-08-19 10:13:46 -0300

    :rocket: added login page

---
+++ src/pages/register.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>B&S Ads</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/register.css">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  </head>
+  <body>
+    <div class="container">
+      <div class="login-body">
+        <form class="form-body" method="POST">
+          <div class="login-title">Acessar conta</div>
+
+          <input class="input" type="email" name="email" placeholder="Email">
+          <i class="fa fa-envelope icon"></i>
+
+          <input class="input" type="password" name="password" maxlength="8" placeholder="Password">
+          <i class="fa fa-lock icon"></i>
+
+          <div class="button-submit"><button>Login</button></div>
+
+          <div class="forgot-user-password">
+            <span>Forgot</span>
+            <a href="reset.php">Username / Password?</a>
+          </div>
+        </form>
+      </div>
+    </div>
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="..assets/js/jquery.min.js"></script>
+    <script src="..assets/js/bootstrap.bundle.min.js"></script>
+    <script src="..assets/js/script.js"></script>
+  </body>
+</html>
